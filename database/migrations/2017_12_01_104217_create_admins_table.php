356e3569530a1aed76fb275ed4f1eef080b191ce
Grad Vasile <grad_simplu@yahoo.com>
2017-12-01 13:42:07 +0200

    first commit

---
+++ database/migrations/2017_12_01_104217_create_admins_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('username', 15)->unique();
+            $table->string('email', 60)->unique();
+            $table->string('password', 60);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
